079846be9e8ce3c67668751a4c5be923fa627435
zw1454 <32356773+zw1454@users.noreply.github.com>
2018-03-25 00:16:02 +0800

    Add files via upload

---
+++ FireAlert_BT.ino
@@ -0,0 +1,116 @@
+
+#include <CurieBLE.h>
+
+const int TrigPin = 2; 
+const int EchoPin = 4; 
+float cm;
+String direction;
+
+int pinBuzzer = 7;
+
+void setup() {
+  Serial.begin(9600);    // initialize serial communication
+  // pinMode(pinBuzzer, OUTPUT);   
+  pinMode(TrigPin, OUTPUT); 
+  pinMode(EchoPin, INPUT);
+  direction = "left";
+  BLE.begin();
+
+  /* Set a local name for the BLE device
+     This name will appear in advertising packets
+     and can be used by remote devices to identify this BLE device
+     The name can be changed but maybe be truncated based on space left in advertisement packet
+  */
+  BLE.setLocalName("Buzzer");
+
+  /* Start advertising BLE.  It will start continuously transmitting BLE
+     advertising packets and will be visible to remote BLE central devices
+     until it receives a new connection */
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+void loop() {
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+  
+    // digitalWrite(pinBuzzer, HIGH);
+    pinMode(pinBuzzer, OUTPUT);
+    
+    float dist = distance();
+    while (central.connected() && dist>50){
+      digitalWrite(pinBuzzer, LOW);
+      delay(100);
+      digitalWrite(pinBuzzer, HIGH);
+      delay(100);
+      dist = distance();
+    }
+    digitalWrite(pinBuzzer, LOW);
+    delay(1500);
+    digitalWrite(pinBuzzer, HIGH);
+    delay(300);
+    while (dist < 50){
+      Direction(direction);
+      dist = distance();
+    }
+  }
+    // when the central disconnects, turn off the LED:
+    // digitalWrite(13, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+}
+
+float distance(){
+  digitalWrite(TrigPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(TrigPin, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(TrigPin, LOW);
+  cm = pulseIn(EchoPin, HIGH) / 58; //将回波时间换算成cm
+  cm = (int(cm * 100)) / 100; //保留两位小数 
+  Serial.print(cm); 
+  Serial.print("cm");//串口输出 
+  Serial.println(); 
+  return cm;
+}
+
+void Direction(String dir){
+  if (dir == "left"){
+      digitalWrite(pinBuzzer, LOW);
+      delay(30);
+      digitalWrite(pinBuzzer,HIGH);
+      delay(500);
+  }
+  if (dir == "right"){
+    digitalWrite(pinBuzzer, LOW);
+    delay(30);
+    digitalWrite(pinBuzzer,HIGH);
+    delay(100);
+    digitalWrite(pinBuzzer, LOW);
+    delay(30);
+    digitalWrite(pinBuzzer,HIGH);
+    delay(250);
+  }
+  if (dir == "exit"){
+    digitalWrite(pinBuzzer, LOW);
+    delay(25);
+    digitalWrite(pinBuzzer,HIGH);
+    delay(75);
+    digitalWrite(pinBuzzer, LOW);
+    delay(25);
+    digitalWrite(pinBuzzer,HIGH);
+    delay(75);
+    digitalWrite(pinBuzzer, LOW);
+    delay(25);
+    digitalWrite(pinBuzzer,HIGH);
+    delay(300);
+  }
+}
